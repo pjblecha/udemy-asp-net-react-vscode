928e9907d70f0f54bae6ff43f8ae4eb5377cc4e5
pjblecha <44296698+pjblecha@users.noreply.github.com>
2025-06-02 16:12:30 -0500

    added db repo accessors class

---
+++ Data/PostsRepository.cs
@@ -0,0 +1,71 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetserver.Data
+{
+    internal static class PostsRepository
+    {
+        internal async static Task<List<Post>> GetPostsAsync()
+        {
+            using (var db = new AppDBContext())
+            {
+                return await db.Posts.ToListAsync();
+            }
+        }
+
+        internal async static Task<Post> GetPostByIdAsync(int postID)
+        {
+            using (var db = new AppDBContext)
+            {
+                return await db.Posts.FirstOrDefaultAsync(post => post.PostID == postID);
+            }
+        }
+
+        internal async static Task<bool> CreatePostAsync(Post toCreate)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    await db.Posts.AddAsync(toCreate);
+                    return true;
+                }
+                catch (System.Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        internal async static Task<bool> UpdatePostAsync(Post toUpdate)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    db.Posts.Update(toUpdate);
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (System.Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        internal async static Task<bool> DeletePostAsync(int postID)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    db.Remove(await GetPostByIdAsync(postID));
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (System.Exception)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
